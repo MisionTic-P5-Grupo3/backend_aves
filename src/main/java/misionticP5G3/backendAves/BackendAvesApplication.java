700e0b4d3ff4bc8edbe783230b5ee317623ba182
Maria Gonzalez <mayigd0502@gmail.com>
2021-11-23 09:57:32 -0500

    Initial settings

---
+++ src/main/java/misionticP5G3/backendAves/BackendAvesApplication.java
@@ -0,0 +1,13 @@
+package misionticP5G3.backendAves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAvesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAvesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/misionticP5G3/example/backendAves/BackendAvesApplicationTests.java
@@ -0,0 +1,13 @@
+package misionticP5G3.example.backendAves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAvesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
